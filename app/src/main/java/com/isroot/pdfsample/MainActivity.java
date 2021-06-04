2a915daa0a8e5af02681f81337562dd634e1efa5
rootBear <dlwodnjs0727@gmail.com>
2021-06-04 13:55:56 +0900

    first commit

---
+++ app/src/main/java/com/isroot/pdfsample/MainActivity.java
@@ -0,0 +1,73 @@
+package com.isroot.pdfsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.gkemon.XMLtoPDF.PdfGenerator;
+import com.gkemon.XMLtoPDF.PdfGeneratorListener;
+import com.gkemon.XMLtoPDF.model.FailureResponse;
+import com.gkemon.XMLtoPDF.model.SuccessResponse;
+import com.isroot.pdfsample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        Log.d("isroot", Environment.getExternalStorageDirectory().getAbsolutePath());
+
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View pdfFirstPage = inflater.inflate(R.layout.pdf_first_page, null);
+        binding.makePdf.setOnClickListener(view -> {
+            PdfGenerator.getBuilder()
+                    .setContext(getApplicationContext())
+                    .fromViewSource()
+                    .fromView(pdfFirstPage)
+                    .setFileName("Test-PDF")
+                    .setFolderName("PDF_test")
+                    .openPDFafterGeneration(true)
+                    .build(new PdfGeneratorListener() {
+                        @Override
+                        public void onFailure(FailureResponse failureResponse) {
+                            super.onFailure(failureResponse);
+                            Log.e("isroot", "onFailure : "+failureResponse.getErrorMessage());
+                            failureResponse.getThrowable().printStackTrace();
+                        }
+
+                        @Override
+                        public void onStartPDFGeneration() {
+                            Log.d("isroot", "startPDFGeneration");
+                        }
+
+                        @Override
+                        public void onFinishPDFGeneration() {
+                            Log.d("isroot", "finishPDFGeneration");
+                        }
+
+                        @Override
+                        public void showLog(String log) {
+                            super.showLog(log);
+                        }
+
+                        @Override
+                        public void onSuccess(SuccessResponse response) {
+                            super.onSuccess(response);
+                            Log.d("isroot", "successPDFGeneration : "+response.getPath());
+                        }
+                    });
+        });
+    }
+}
---
+++ app/src/main/java/com/isroot/pdfsample/MainVIewModel.java
@@ -0,0 +1,12 @@
+package com.isroot.pdfsample;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+public class MainVIewModel extends AndroidViewModel {
+    public MainVIewModel(@NonNull Application application) {
+        super(application);
+    }
+}
